05cbe7a84f5f7ff28fb90f32e61d93b8c69e5622
ScrimmyFS <dscrimshaw12345@gmail.com>
2023-08-16 17:36:16 -0400

    first commit

---
+++ constants/index.js
@@ -0,0 +1,94 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world and always remember to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+      'We have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+      'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes will change the world',
+    subtitle:
+      'The concept of the metaverse has the potential to bring about significant changes to the world across various domains. While its important to note that the metaverse is still largely in its conceptual and early development stages, there are several ways in which it could impact society, technology, and various industries.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Tips to easily master the madness of the Metaverse',
+    subtitle:
+      'Set Boundaries: Establish clear limits on your time spent in the metaverse to maintain a healthy balance between virtual and real life Protect Privacy: Be mindful of the personal information you share and the permissions you grant while interacting in virtual these spaces. Prioritize Real Connections: While the metaverse offers new social opportunities, dont neglect face- to - face interactions and relationships in the physical world.' 
+  },
+{
+  imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+      subtitle:
+  'Take it on and see what you can achieve',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
+
+
+
+
+
+
